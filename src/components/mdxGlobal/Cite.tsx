cfd8f6a5b106385f80c9f01f2ee70fa58087a5da
0TickPulse <62788361+0tickpulse@users.noreply.github.com>
2024-02-29 21:35:31 +0800

    add new stuff

---
+++ src/components/mdxGlobal/Cite.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import HoverableBox from "./HoverableBox";
+
+export type CiteProps = {
+    /**
+     * The id of the citation, used for linking.
+     */
+    id: string;
+
+    page?: number | [number, number];
+};
+
+export default function Cite(props: CiteProps) {
+    // format: (id) or (id, page)
+    // page can be a single number or a range
+    // range is formatted as start-end
+    const text = (
+        <>
+            Cite: {props.id}
+            {props.page && <>, {Array.isArray(props.page) ? props.page[0] + "-" + props.page[1] : props.page}</>}
+        </>
+    );
+    const link = "#" + props.id;
+
+    return (
+        <HoverableBox
+            triggerText={text}
+            href={link}
+            // green color
+            bgColor="bg-emerald-500 bg-opacity-50"
+            textColor="text-black"
+            darkBgColor="dark:bg-emerald-500"
+            darkTextColor="dark:text-white"
+        />
+    );
+}
---
+++ src/theme/MDXComponents.ts
@@ -0,0 +1,11 @@
+import MDXComponents from "@theme-original/MDXComponents";
+import Citation from "@site/src/components/mdxGlobal/Citation";
+import Cite from "@site/src/components/mdxGlobal/Cite";
+import HoverableBox from "@site/src/components/mdxGlobal/HoverableBox";
+
+export default {
+    ...MDXComponents,
+    Citation,
+    Cite,
+    HoverableBox,
+};
