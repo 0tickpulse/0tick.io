e9214706a4a5a50e10f6d7bb60c188eba6bd8a95
0tickpulse <62788361+0tickpulse@users.noreply.github.com>
2024-02-29 10:16:29 +0800

    change edit urls

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -60,7 +60,7 @@ const config = {
                     sidebarPath: "./sidebars.ts",
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+                    editUrl: "https://github.com/0tickpulse/0tick.io/tree/main/",
                     routeBasePath: "/",
                     remarkPlugins: [remarkMath],
                     rehypePlugins: [rehypeKatex],
@@ -69,7 +69,7 @@ const config = {
                     showReadingTime: true,
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+                    editUrl: "https://github.com/0tickpulse/0tick.io/tree/main/",
                 },
                 theme: {
                     customCss: require.resolve("./src/css/custom.scss"),
