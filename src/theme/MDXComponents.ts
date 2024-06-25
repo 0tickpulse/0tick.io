955ca9bd19571dbd89e0d5ddb840c54272e766db
0TickPulse <62788361+0tickpulse@users.noreply.github.com>
2024-06-26 01:48:56 +0800

    major math notes added, RiemannApproximation component

--- src/theme/MDXComponents.ts
+++ src/theme/MDXComponents.ts
@@ -3,11 +3,17 @@ import Citation from "@site/src/components/mdxGlobal/Citation";
 import Cite from "@site/src/components/mdxGlobal/Cite";
 import HoverableBox from "@site/src/components/mdxGlobal/HoverableBox";
 import MDXComponents from "@theme-original/MDXComponents";
+import { DerivativeFromLimits } from "@site/src/components/mdxGlobal/Differentiation";
+import { RiemannApproximation } from "@site/src/components/mdxGlobal/RiemannApproximation";
+import { ThreeDTest } from "@site/src/components/mdxGlobal/ThreeDTest";
 
 export default {
     ...MDXComponents,
+    Boxed,
     Citation,
     Cite,
+    DerivativeFromLimits,
     HoverableBox,
-    Boxed,
+    RiemannApproximation,
+    ThreeDTest,
 };
