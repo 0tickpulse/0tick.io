9da3cd57bc2406b3ad51b8808e61ad4044282994
0TickPulse <62788361+0tickpulse@users.noreply.github.com>
2024-06-26 01:47:50 +0800

    add katex macros

---
+++ katexMacros.ts
@@ -0,0 +1,14 @@
+export const KATEX_MACROS: Record<string, string> = {
+    "\\ihat": "\\hat{\\mathbf{i}}",
+    "\\jhat": "\\hat{\\mathbf{j}}",
+    "\\khat": "\\hat{\\mathbf{k}}",
+    "\\rhat": "\\hat{\\mathbf{r}}",
+    "\\thetahat": "\\hat{\\boldsymbol{\\theta}}",
+    "\\unitvec": "\\hat{\\mathbf{#1}}",
+    "\\arrowedvec": "\\overrightarrow{#1}",
+    "\\vec": "\\mathbf{#1}",
+
+    "\\arcsec": "\\operatorname{arcsec}",
+    "\\arccsc": "\\operatorname{arccsc}",
+    "\\arccot": "\\operatorname{arccot}",
+};
