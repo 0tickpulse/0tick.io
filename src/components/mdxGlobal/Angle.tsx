2b2b66b619e01dcc4b845adad656aed65c98ad9e
0TickPulse <62788361+0tickpulse@users.noreply.github.com>
2024-06-28 03:21:25 +0800

    add diff eq, fix stuff with Angle

--- src/components/mdxGlobal/Angle.tsx
+++ src/components/mdxGlobal/Angle.tsx
@@ -1,31 +1,51 @@
-import { LaTeX, Line, Plot, Polygon } from "mafs";
+import { LaTeX, Line, Plot, vec, Polygon, useTransformContext, Transform } from "mafs";
 
 export type AngleProps = {
     at: [number, number];
     fromRad: number;
     toRad: number;
     radius?: number;
+    color?: string;
     straightRightAngle?: boolean;
+    /**
+     * Use if you know the angle is a right angle.
+     * Sometimes the angle is not exactly 90 degrees due to floating point errors.
+     */
+    forceStraightAngle?: boolean;
     /**
      * Label using LaTeX syntax
      */
     label?: string;
+    labelColor?: string;
     labelOffsetMultiplier?: number;
 };
 
-export function Angle({ at, fromRad, toRad, radius = 0.4, straightRightAngle = true, label, labelOffsetMultiplier = 1.4 }: AngleProps) {
-    if (straightRightAngle && Math.abs(toRad - fromRad) === Math.PI / 2) {
-        const p1 = [Math.cos(fromRad) * radius + at[0], Math.sin(fromRad) * radius + at[1]];
-        const p2 = [Math.cos(toRad) * radius + at[0], Math.sin(fromRad) * radius + at[1]];
-        const p3 = [Math.cos(toRad) * radius + at[0], Math.sin(toRad) * radius + at[1]];
-        const texPosition = [p2[0], p2[1]];
-        texPosition[0] = (texPosition[0] - at[0]) * labelOffsetMultiplier + at[0];
-        texPosition[1] = (texPosition[1] - at[1]) * labelOffsetMultiplier + at[1];
+export function Angle({
+    at,
+    fromRad,
+    toRad,
+    radius = 0.4,
+    color,
+    straightRightAngle = true,
+    forceStraightAngle = false,
+    label,
+    labelColor = color,
+    labelOffsetMultiplier = 1.4,
+}: AngleProps) {
+    const { userTransform } = useTransformContext();
+    if ((straightRightAngle && Math.abs(toRad - fromRad) === Math.PI / 2) || forceStraightAngle) {
+        if (fromRad > toRad) {
+            [fromRad, toRad] = [toRad, fromRad];
+        }
+        if (toRad - fromRad > Math.PI) {
+            [fromRad, toRad] = [toRad, fromRad];
+        }
         return (
             <>
-                <Line.Segment point1={p1} point2={p2} />
-                <Line.Segment point1={p2} point2={p3} />
-                {label && <LaTeX tex={label} at={texPosition} />}
+                <Transform rotate={fromRad} translate={at}>
+                    <Line.Segment point1={[radius, 0]} point2={[radius, radius]} />
+                    <Line.Segment point1={[0, radius]} point2={[radius, radius]} />
+                </Transform>
             </>
         );
     }
@@ -39,8 +59,8 @@ export function Angle({ at, fromRad, toRad, radius = 0.4, straightRightAngle = t
 
     return (
         <>
-            <Plot.Parametric xy={parametric} t={[fromRad, toRad]} />
-            {label && <LaTeX tex={label} at={texPosition} />}
+            <Plot.Parametric xy={(t) => vec.transform(parametric(t), userTransform)} t={[fromRad, toRad]} color={color} />
+            {label && <LaTeX tex={label} at={texPosition} color={labelColor} />}
         </>
     );
 }
