1153a0597b86cf8592e7c8c7841fd6c685c78aa3
0TickPulse <62788361+0tickpulse@users.noreply.github.com>
2024-06-27 22:26:53 +0800

    fix macros

--- katexMacros.ts
+++ katexMacros.ts
@@ -1,18 +1,14 @@
-export const KATEX_MACROS: Record<string, string> = {
-    "\\ihat": "\\hat{\\mathbf{i}}",
-    "\\jhat": "\\hat{\\mathbf{j}}",
-    "\\khat": "\\hat{\\mathbf{k}}",
-    "\\rhat": "\\hat{\\mathbf{r}}",
-    "\\thetahat": "\\hat{\\boldsymbol{\\theta}}",
-    "\\unitvec": "\\hat{\\mathbf{#1}}",
-    "\\arrowedvec": "\\overrightarrow{#1}",
-    "\\vec": "\\mathbf{#1}",
+export const MATH_MACROS: Record<string, string | [string, number]> = {
+    "ihat": "\\hat{\\mathbf{i}}",
+    "jhat": "\\hat{\\mathbf{j}}",
+    "khat": "\\hat{\\mathbf{k}}",
+    "rhat": "\\hat{\\mathbf{r}}",
+    "thetahat": "\\hat{\\boldsymbol{\\theta}}",
+    "unitvec": ["\\hat{\\mathbf{#1}}", 1],
+    "arrowedvec": ["\\overrightarrow{#1}", 1],
+    "vec": ["\\mathbf{#1}", 1],
 
-    "\\arcsec": "\\operatorname{arcsec}",
-    "\\arccsc": "\\operatorname{arccsc}",
-    "\\arccot": "\\operatorname{arccot}",
-
-    "\\eqref": "\\href{###1}{(\\text{#1})}",
-    "\\ref": "\\href{###1}{\\text{#1}}",
-    "\\label": "\\htmlId{#1}{}"
+    "arcsec": "\\operatorname{arcsec}",
+    "arccsc": "\\operatorname{arccsc}",
+    "arccot": "\\operatorname{arccot}",
 };
