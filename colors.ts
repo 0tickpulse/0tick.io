a9b4eaa93d55e2df63294a143a9a737be8004d9a
0TickPulse <62788361+0tickpulse@users.noreply.github.com>
2024-06-28 15:27:40 +0800

    temporarily add colors constant to typescript to fix hydration issue

---
+++ colors.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+    gray: "#44546A",
+    blue: "#0194FE",
+    red: "#FE2E46",
+    yellow: "#FFC51D",
+    green: "#7EF84A",
+    aqua: "#00D39D",
+    purple: "#B467F5",
+};
