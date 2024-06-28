0e9826cebf6639e4f16e5bf705659c98b57a8bd4
0TickPulse <62788361+0tickpulse@users.noreply.github.com>
2024-06-28 15:27:40 +0800

    temporarily add colors constant to typescript to fix hydration issue

--- src/utilities/colors.ts
+++ src/utilities/colors.ts
@@ -1,4 +1,7 @@
+import { COLORS } from "@site/colors";
+
 export function color(name: string) {
     // get CSS variable --custom-color-<name>
-    return getComputedStyle(document.documentElement).getPropertyValue(`--custom-color-${name}`);
+    // return getComputedStyle(document.documentElement).getPropertyValue(`--custom-color-${name}`);
+    return COLORS[name];
 }
