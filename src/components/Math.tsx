3abc76ab4acee1776a0b02a7c24aa34656291e31
0TickPulse <62788361+0tickpulse@users.noreply.github.com>
2024-07-03 13:59:06 +0800

    refactor Math to catch errors and useDocusaurusContext

--- src/components/Math.tsx
+++ src/components/Math.tsx
@@ -1,24 +1,44 @@
-import config from "@site/docusaurus.config";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import { TeX } from "mathjax-full/js/input/tex";
+import config from "@site/docusaurus.config";
 import { AllPackages } from "mathjax-full/js/input/tex/AllPackages";
 import { mathjax } from "mathjax-full/js/mathjax";
 import { SVG } from "mathjax-full/js/output/svg";
-import { CHTML } from "mathjax-full/js/output/chtml";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { Options } from "rehype-mathjax";
+import { isEqual } from "lodash";
 
 export default function MathEquation({ children, ...options }: { children: ReactNode } & Options): JSX.Element {
+    const siteConfig = useDocusaurusContext().siteConfig;
     const newOptions = {
-        ...config.customFields.mathJaxOptions,
+        ...siteConfig.customFields.mathJaxOptions as Options,
         ...options,
     };
     const input = new TeX({
         packages: AllPackages,
-        ...options.tex,
+        ...newOptions.tex,
     });
     const output = new SVG(newOptions.svg || undefined);
-    // const output = new CHTML(newOptions.chtml || undefined);
-    const doc = mathjax.document("", { InputJax: input, OutputJax: output });
-    const domNode: HTMLElement = doc.convert(children.toString(), newOptions);
-    return <div dangerouslySetInnerHTML={{ __html: domNode.outerHTML }} />;
+    // co`nst output = new CHTML(newOptions.chtml || undefined);
+    try {
+        const doc = mathjax.document("", { InputJax: input, OutputJax: output });
+        const domNode: HTMLElement = doc.convert(children.toString(), {});
+        return <div dangerouslySetInnerHTML={{ __html: domNode.outerHTML }} />;
+    } catch (e) {
+        console.log(`Error in MathJax: ${e}`, {
+            newOptions,
+            siteConfig: siteConfig.customFields.mathJaxOptions,
+            config: config.customFields.mathJaxOptions,
+            eq: isEqual(siteConfig.customFields.mathJaxOptions, config.customFields.mathJaxOptions),
+        });
+    }
+    // const doc = mathjax.document("", { InputJax: input, OutputJax: output });
+
+    // const [html, setHtml] = useState<string>("");
+
+    // useEffect(() => {
+    //     setHtml(domNode.outerHTML);
+    // }, [domNode]);
+
+    return <div />;
 }
