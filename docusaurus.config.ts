d5f6788061affda8cf4f117e57cc126ed07c2b11
0TickPulse <62788361+0tickpulse@users.noreply.github.com>
2024-07-08 17:57:33 +0800

    finish differential equations

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -21,7 +21,7 @@ const config = {
             tex: {
                 tags: "ams",
                 macros: MATH_MACROS,
-                packages: { "[+]": ["autoload", "gensymb", "ams", "cancel", "mathtools", "html", "configmacros", "physics"] },
+                packages: { "[+]": ["autoload", "mhchem", "gensymb", "ams", "cancel", "mathtools", "html", "configmacros", "physics"] },
             },
             svg: {
                 scale: 1.2,
--- src/components/Math.tsx
+++ src/components/Math.tsx
@@ -8,10 +8,10 @@ import { ReactNode, useEffect, useState } from "react";
 import { Options } from "rehype-mathjax";
 import { isEqual } from "lodash";
 
-export default function MathEquation({ children, ...options }: { children: ReactNode } & Options): JSX.Element {
+export default function MathEquation({ children, inline = false, ...options }: { children: ReactNode; inline: boolean } & Options): JSX.Element {
     const siteConfig = useDocusaurusContext().siteConfig;
     const newOptions = {
-        ...siteConfig.customFields.mathJaxOptions as Options,
+        ...(siteConfig.customFields.mathJaxOptions as Options),
         ...options,
     };
     const input = new TeX({
@@ -22,7 +22,10 @@ export default function MathEquation({ children, ...options }: { children: React
     // co`nst output = new CHTML(newOptions.chtml || undefined);
     try {
         const doc = mathjax.document("", { InputJax: input, OutputJax: output });
-        const domNode: HTMLElement = doc.convert(children.toString(), {});
+        console.log({ children: children.toString() });
+        const domNode: HTMLElement = doc.convert(children.toString(), {
+            display: !inline,
+        });
         return <div dangerouslySetInnerHTML={{ __html: domNode.outerHTML }} />;
     } catch (e) {
         console.log(`Error in MathJax: ${e}`, {
