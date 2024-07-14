0e329160cf9ab91c87e5df5e750c6e968cf691ad
0tickpulse <62788361+0tickpulse@users.noreply.github.com>
2024-07-14 21:39:15 +0800

    more work on linear alg and calc 3

--- src/components/mdxGlobal/LinearTransformationVisualizer.tsx
+++ src/components/mdxGlobal/LinearTransformationVisualizer.tsx
@@ -1,12 +1,50 @@
-import { Coordinates, Mafs, Transform, useMovablePoint, useTransformContext, vec, Vector } from "mafs";
+import { Coordinates, Mafs, Transform, useMovablePoint, useStopwatch, useTransformContext, vec, Vector } from "mafs";
 import { color } from "@site/src/utilities/colors";
+import { easeInOutCubic } from "js-easing-functions"
+import { useEffect, useState } from "react";
 
 export type LinearTransformationVisualizerProps = {
+    from?: LinearTransformation;
+    to: LinearTransformation;
+    children?: React.ReactNode;
+};
+
+export type LinearTransformation = {
     ihat: vec.Vector2;
     jhat: vec.Vector2;
 };
 
-export default function LinearTransformationVisualizer({ ihat = [1, 0], jhat = [0, 1] }: LinearTransformationVisualizerProps) {
+const LINEAR_TRANSFORMATION_DEFAULT: LinearTransformation = {
+    ihat: [1, 0],
+    jhat: [0, 1],
+};
+
+export default function LinearTransformationVisualizer({ from = LINEAR_TRANSFORMATION_DEFAULT, to, children }: LinearTransformationVisualizerProps) {
+    const { time, start, stop } = useStopwatch({ endTime: 2 });
+    const T = easeInOutCubic(time, 0, 1, 2);
+    const ihat = vec.lerp(from.ihat, to.ihat, T);
+    const jhat = vec.lerp(from.jhat, to.jhat, T);
+
+    const [hasStarted, setHasStarted] = useState(false);
+    const [hasTouched, setHasTouched] = useState(false);
+    function startStop() {
+        setHasTouched(true);
+        if (hasStarted) {
+            stop();
+        } else {
+            start();
+        }
+        setHasStarted(!hasStarted);
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (hasTouched && !hasStarted) {
+                startStop();
+            }
+        }, 1000);
+    }, []);
+
     const matrix = [ihat[0], -ihat[1], jhat[0], -jhat[1], 0, 0];
 
     const transformedCartesian = (
@@ -21,19 +59,33 @@ export default function LinearTransformationVisualizer({ ihat = [1, 0], jhat = [
     );
 
     return (
-        <>
-            <g
-                transform={`matrix(${matrix.join(" ")})`}
-                style={
-                    {
-                        "--mafs-line-color": "white",
-                    } as React.CSSProperties
-                }
-            >
-                {transformedCartesian}
-            </g>
-            <Vector tail={[0, 0]} tip={ihat} color={color("blue")} />
-            <Vector tail={[0, 0]} tip={jhat} color={color("yellow")} />
-        </>
+        <div className="card">
+            <div className="card__body">
+                <Mafs>
+                    <Coordinates.Cartesian />
+                    <g
+                        transform={`matrix(${matrix.join(" ")})`}
+                        style={
+                            {
+                                "--mafs-line-color": "white",
+                            } as React.CSSProperties
+                        }
+                    >
+                        {transformedCartesian}
+                    </g>
+                    <Vector tail={[0, 0]} tip={ihat} color={color("blue")} />
+                    <Vector tail={[0, 0]} tip={jhat} color={color("yellow")} />
+                    {children}
+                </Mafs>
+            </div>
+            <div className="card__footer">
+                <button
+                    className={`button button--secondary button--block ${hasStarted ? "button--danger" : "button--success"}`}
+                    onClick={startStop}
+                >
+                    {hasStarted ? "Reset" : "Start"}
+                </button>
+            </div>
+        </div>
     );
 }
--- src/utilities/colors.ts
+++ src/utilities/colors.ts
@@ -5,3 +5,72 @@ export function color(name: string) {
     // return getComputedStyle(document.documentElement).getPropertyValue(`--custom-color-${name}`);
     return COLORS[name];
 }
+
+/**
+ * @param h From 0 to 1
+ * @returns
+ */
+export function HSVtoRGB(h: number, s: number, v: number) {
+    let r: number, g: number, b: number;
+    let i: number, f: number, p: number, q: number, t: number;
+
+    if (s === 0) {
+        r = g = b = v;
+        return [r, g, b];
+    }
+
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+
+    switch (i % 6) {
+        case 0:
+            r = v;
+            g = t;
+            b = p;
+            break;
+        case 1:
+            r = q;
+            g = v;
+            b = p;
+            break;
+        case 2:
+            r = p;
+            g = v;
+            b = t;
+            break;
+        case 3:
+            r = p;
+            g = q;
+            b = v;
+            break;
+        case 4:
+            r = t;
+            g = p;
+            b = v;
+            break;
+        case 5:
+            r = v;
+            g = p;
+            b = q;
+            break;
+    }
+
+    return [r, g, b];
+}
+
+export function getColorFromMagnitude(magnitude: number, minMagnitude = 0, maxMagnitude = 20) {
+    // Normalize the magnitude to a 0-1 scale
+    const normalizedMagnitude = (magnitude - minMagnitude) / (maxMagnitude - minMagnitude);
+
+    // Convert the normalized magnitude to an HSV color
+    // HSV spectrum is from 241/360 (small) to 0 (big)
+    const hue = Math.max(0, 241 / 360 * (1 - normalizedMagnitude));
+    const saturation = 1;
+    const value = 1;
+    const [red, green, blue] = HSVtoRGB(hue, saturation, value);
+
+    return [red, green, blue];
+}
